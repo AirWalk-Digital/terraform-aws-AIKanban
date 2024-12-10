7881bdac36bd020edce810ea922b6423b223f73b
Josh Prout <jnp207@exeter.ac.uk>
2024-12-10 16:43:59 +0000

    Create API gateway cloudwatch push role

--- layers/lambda/iam.tf
+++ layers/lambda/iam.tf
@@ -1,4 +1,6 @@
 
+# -------- lambda -----------
+
 resource "aws_iam_policy" "lambda_bedrock_invocation_policy" {
   name        = "${var.environment}-${var.project}-lambda-bedrock-invoc-policy"
   path        = "/"
@@ -58,3 +60,37 @@ resource "aws_lambda_permission" "apigw_lambda" {
 
   source_arn = "${aws_api_gateway_rest_api.rest_api.execution_arn}/*/*/*"
 }
+
+# --------- API gateway -----------
+
+resource "aws_iam_role" "apigateway_cloudwatch_role" {
+  name        = "${var.environment}-${var.project}-apigw-cw-role"
+  path        = "/"
+  description = "Gives API gateway write access to cloudwatch logs"
+
+  assume_role_policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Sid" : "",
+          "Effect" : "Allow",
+          "Principal" : {
+            "Service" : [
+              "apigateway.amazonaws.com"
+            ]
+          },
+          "Action" : [
+            "sts:AssumeRole"
+          ]
+        }
+      ]
+    }
+  )
+}
+
+resource "aws_iam_role_policy_attachment" "apigateway_cw_attachment" {
+  role       = aws_iam_role.apigateway_cloudwatch_role
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonAPIGatewayPushToCloudWatchLogs"
+}
+
