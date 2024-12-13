2742a802262c255b76b2dfc00aa569f6e9ddce2a
Josh Prout <jnp207@exeter.ac.uk>
2024-12-13 14:21:18 +0000

    Add HTTP error responses to lambda function

--- layers/lambda/python/index.py
+++ layers/lambda/python/index.py
@@ -8,60 +8,32 @@ from botocore.exceptions import ClientError
 client = boto3.client(service_name="bedrock-runtime", region_name="eu-west-2")
 
 def lambda_handler(event, context):
-    
-    model = "claude"
-
-    if model == "titan":
-
-        prompt = "Your role is to take a transcript of a meeting, and output it in the form of Kanban board items, formatted in JSON. Only output JSON formatted actions. Focus on tasks that need to be completed, not on what has been completed already. This is the transcript:" + event["prompt"]
-
-        native_request = {
-            "inputText": prompt,
-            "textGenerationConfig": {
-                "maxTokenCount": 512,
-                "temperature": 0.2,
-            },
-        }
-
-        request = json.dumps(native_request)
-
-
-        try:
-            response = client.invoke_model(modelId="amazon.titan-text-express-v1", body=request)
-
-        except (ClientError, Exception) as e:
-            print(f"ERROR: Can't invoke: Reason: {e}")
-            exit(1)
-
-        model_response = json.loads(response["body"].read())
-
-        response_text = model_response["results"][0]["outputText"]
-
-
-        return {
-            'statusCode': 200,
-            'body': json.dumps({"Answer": response_text})
-        }
-    
-    elif model == "claude":
+            
+    try:        
+        body = json.loads(event["body"])
+        prompt = body["prompt"]
         
-        prompt = event["prompt"]
+    except Exception as err:
+        return{
+            'statusCode':400,
+            'body': "Request body missing parameter 'prompt' "}
 
-        model_id = "anthropic.claude-3-haiku-20240307-v1:0"
 
-        f = open("system_prompt2.txt", "r")
-        
+    model_id = "anthropic.claude-3-haiku-20240307-v1:0"
 
-        system_prompt = f.read()
+    f = open("system_prompt2.txt", "r")
+    system_prompt = f.read()
 
+
+    try:
         response = client.invoke_model(
             modelId = model_id,
             body=json.dumps(
                 {"anthropic_version": "bedrock-2023-05-31",
-                 "max_tokens": 1024,
-                 "system":system_prompt,
-                 "messages": [
-                 {
+                    "max_tokens": 1024,
+                    "system":system_prompt,
+                    "messages": [
+                    {
                     "role": "user",
                     "content": [
                         {
@@ -72,18 +44,31 @@ def lambda_handler(event, context):
                 }
             ],
         }))
-
         model_response = json.loads(response["body"].read())
 
-        print(model_response)
-
-        response_text = model_response["content"][0]["text"]
-
-
+    except:
         return {
-            'statusCode': 200,
-            'body': json.dumps({"Answer": response_text})
+            'statusCode' : 500,
+            'body' : "Model invocation failed"
         }
+
+    print(model_response)
+
+    response = model_response["content"][0]["text"]
+    
+    try:
+        suggestions = response["suggestions"]
+
+    except Exception as err:
+        return {
+            'statusCode' : 500,
+            'body' : str(err)
+        }
+
+    return {
+        'statusCode': 200,
+        'body': suggestions
+    }
 
 
 
