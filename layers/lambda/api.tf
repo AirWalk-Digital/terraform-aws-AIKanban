8696607d37c0d730a7f8edb304eeed5ada5bb4a6
Josh Prout <jnp207@exeter.ac.uk>
2024-12-13 12:25:33 +0000

    Update api gateway to use Lambda Proxy integration

--- layers/lambda/api.tf
+++ layers/lambda/api.tf
@@ -31,7 +31,7 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   resource_id             = aws_api_gateway_resource.root.id
   http_method             = aws_api_gateway_method.proxy.http_method
   integration_http_method = "POST"
-  type                    = "AWS"
+  type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.lambda_bedrock_invoc.invoke_arn
 }
 
@@ -55,6 +55,10 @@ resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
   triggers = {
     redeployment = timestamp()
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
